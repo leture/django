a8a8b9a8e463ae017263606913b6d5817578f9e3
Steffen Jasper <jesus_at_hollywoodparty@gmx.net>
2015-11-25 11:33:00 +0100

    [1.4.x] Bumped version for 1.4.23 release

--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,9 @@ copyright = 'Django Software Foundation and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.22'
+version = '1.4.23'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.22'
+release = '1.4.23'
 # The next version to be released
 django_next_version = '1.5'
 
