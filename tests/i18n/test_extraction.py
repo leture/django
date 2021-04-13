296fa63c2a5857283f98f2a0c32a6da723e5dd8f
Steffen Jasper <steffen.jasper@descript.de>
2021-04-13 09:09:25 +0200

    Removed test which is not triggered by newer gettext versions.

--- tests/i18n/test_extraction.py
+++ tests/i18n/test_extraction.py
@@ -236,15 +236,6 @@ class BasicExtractorTests(ExtractorTests):
         self.assertIn("UnicodeDecodeError: skipped file not_utf8.txt in .",
                       force_text(out.getvalue()))
 
-    def test_extraction_warning(self):
-        """test xgettext warning about multiple bare interpolation placeholders"""
-        os.chdir(self.test_dir)
-        shutil.copyfile('./code.sample', './code_sample.py')
-        self.addCleanup(self.rmfile, os.path.join(self.test_dir, 'code_sample.py'))
-        out = StringIO()
-        management.call_command('makemessages', locale=[LOCALE], stdout=out)
-        self.assertIn("code_sample.py:4", force_text(out.getvalue()))
-
     def test_template_message_context_extractor(self):
         """
         Ensure that message contexts are correctly extracted for the
