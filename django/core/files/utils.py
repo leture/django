d748dae37c1caed8932cddaff2e8e164219676f2
Steffen Jasper <steffen.jasper@descript.de>
2023-03-16 15:06:09 +0100

    Ignore double point in filename

--- django/core/files/utils.py
+++ django/core/files/utils.py
@@ -9,7 +9,7 @@ def validate_file_name(name, allow_relative_path=False):
         raise SuspiciousFileOperation("Could not derive file name from '%s'" % name)
 
     if allow_relative_path:
-        if os.path.isabs(name) or '..' in name:
+        if os.path.isabs(name) or '..' in os.path.dirname(name):
             raise SuspiciousFileOperation("Detected path traversal attempt in '%s'" % name)
     elif name != os.path.basename(name):
         raise SuspiciousFileOperation("File name '%s' includes path elements" % name)
