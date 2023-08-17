66896c12c6bfb5ce28629e2bc2ba4b02581b4bc2
Steffen Jasper <steffen.jasper@descript.de>
2023-08-17 11:40:02 +0200

    Avoid over excessive JOIN usage

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -1017,10 +1017,12 @@ class ModelAdmin(BaseModelAdmin):
         if search_fields and search_term:
             orm_lookups = [construct_search(str(search_field))
                            for search_field in search_fields]
+            bit_queries = []
             for bit in search_term.split():
                 or_queries = [models.Q(**{orm_lookup: bit})
                               for orm_lookup in orm_lookups]
-                queryset = queryset.filter(reduce(operator.or_, or_queries))
+                bit_queries.append(reduce(operator.or_, or_queries))
+            queryset = queryset.filter(reduce(operator.and_, bit_queries))
             use_distinct |= any(lookup_needs_distinct(self.opts, search_spec) for search_spec in orm_lookups)
 
         return queryset, use_distinct
